31a46b3a7c97868f268c2d6cb9d03f816d1b2bfe
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-12-05 16:07:46 +0200

    Add scaffolding for multi-mass integrators, still need to implement discontinous boundary conditions at each mass.

--- src/integrator.py
+++ src/integrator.py
@@ -9,56 +9,101 @@ class SlacklineTooLongError(Exception):
     pass
 
 
-def integrate(lagrangian, slackline, anchor_tension, anchor_angle, length_cutoff=10000.0):
+def integrate(lagrangian, slackline, anchor_tension, anchor_angle, 
+              masses=[], length_cutoff=10000.0):
     """
     Integrates the curve of a rigged slackline with given anchor tension, and
     angle. This can be binary searched to do more interesting things, like
     finding the curve for a slackline of a given gap length.
+
+    Inputs:
+        lagrangian: A sympy expression for the Lagrangian of the system.
+        slackline: A Slackline object that describes the webbing.
+        anchor_tension: The tension in the webbing at the left anchor point.
+        anchor_angle: The angle below horizontal of the webbing at the left anchor point.
+        masses: A list of (x, m) tuples, where x is the position of the mass, and m is the mass. Defaults to the empty list.
+        length_cutoff: The maximum length of the slackline. Defaults to 10000m.
     """
     # The system of four ODEs we need to integrate:
     fn_y, fn_n, fn_y_x, fn_n_x = first_order_euler_lagrange(lagrangian)
+    def fn(x, y):
+        return np.array([
+            fn_y(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
+            fn_n(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
+            fn_y_x(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
+            fn_n_x(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
+        ])
 
-    # Initial conditions for each variable:
+    # Initial conditions for each variable at the left anchor:
     y0 = 0.0
     n0 = 0.0
     y_x0 = np.tan(-anchor_angle)
     n_x0 = np.sqrt(1 + y_x0**2) / (anchor_tension / slackline.K + 1)
 
-    # We're not sure how long the slackline will be, so we need to search for
-    # that, starting with a guess of X=1000m.
-    L = 1000.0
-    while L < length_cutoff:
-        x = np.linspace(0, L, 2000)
-        def fn(x, y):
-            return np.array([
-                fn_y(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
-                fn_n(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
-                fn_y_x(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
-                fn_n_x(x, y[0], y[1], y[2], y[3], slackline.m, slackline.g, slackline.K),
-            ])
-        sol = sp.integrate.solve_ivp(
-            fn,
-            (0, L),
-            (y0, n0, y_x0, n_x0),
-            method="BDF",
-            t_eval=x,
-        )
+    # We treat masses as point particles, which introduce discontinuities in 
+    # the system and require us to integrate the segments of webbing between
+    # them independently.
+    x = np.array([])
+    y = np.array([])
+    n = np.array([])
+    y_x = np.array([])
+    n_x = np.array([])
+    last_x = 0.0
+    for i in range(len(masses) + 1):
+        # If this is the last segment, we integrate until we reach y > 0,
+        # which represents the right anchor point. Otherwise, we integrate
+        # until we reach the next mass:
+        if i == len(masses):
+            L = 1000.0
+        else:
+            L = masses[i][0]
 
-        # If the curve hasn't reached the other anchor, we need to increase
-        # the length of the curve we're integrating by double:
-        if sol.y[0][-1] < 0:
-            L *= 2
-            continue
+        # We use a loop because we need to search for the length of the last
+        # segment of webbing, which we don't know up front:
+        while True:
+            if L + last_x > length_cutoff:
+                raise SlacklineTooLongError("The slackline is too long to integrate.")
 
-        # If the curve has reached the other anchor, we need to cap the arrays
-        # to the correct length:
-        x = sol.t
-        y = sol.y[0]
-        n = sol.y[1]
-        y_x = sol.y[2]
-        n_x = sol.y[3]
-        i = np.argmax(y > 0)
-        return x[:i], y[:i], n[:i], y_x[:i], n_x[:i]
+            _x = np.linspace(last_x, last_x + L, 1000)
+            sol = sp.integrate.solve_ivp(
+                fn,
+                (last_x, last_x + L),
+                (y0, n0, y_x0, n_x0),
+                method="BDF",
+                t_eval=_x,
+            )
 
-    # If we reach this point, we've failed to find the end of the slackline:
-    raise SlacklineTooLongError("The slackline is too long to integrate.")
+            # Path for the last segment:
+            if i == len(masses):
+                # If we haven't reached the right anchor point, we need to
+                # increase the length of the segment:
+                if sol.y[0][-1] < 0.0:
+                    L *= 2
+                    continue
+
+                # Otherwise we need to trim the solution to the right anchor:
+                j = np.argmax(sol.y[0] > 0.0)
+                x = np.append(x, sol.t[:j])
+                y = np.append(y, sol.y[0][:j])
+                n = np.append(n, sol.y[1][:j])
+                y_x = np.append(y_x, sol.y[2][:j])
+                n_x = np.append(n_x, sol.y[3][:j])
+                break
+
+            # Path for an initial segment:
+            x = np.append(x, sol.t)
+            y = np.append(y, sol.y[0])
+            n = np.append(n, sol.y[1])
+            y_x = np.append(y_x, sol.y[2])
+            n_x = np.append(n_x, sol.y[3])
+            last_x = x[-1]
+
+            # TODO: update the initial conditions based on dirac delta stuff
+            y0 = y[-1]
+            n0 = n[-1]
+            y_x0 = y_x[-1]
+            n_x0 = n_x[-1]
+            break
+
+    # ...and we're done here:
+    return x, y, n, y_x, n_x
--- src/test.py
+++ src/test.py
@@ -7,14 +7,15 @@ from .integrator import integrate
 def main():
     # Run the integrator for an ideal dyneemite_pro slackline, for tensions
     # 1000, 1500, 2000, ..., 4000, and angle 0.1,0.2,0.3,0.4,0.5:
-    for T0 in range(1000, 5000, 500):
-        for theta0 in np.linspace(0.1, 0.5, 5):
+    for T0 in [1000, 2000, 3000]:
+        for theta0 in [0.1, 0.2, 0.3]:
             print(f"Integrating for T0={T0}, theta0={theta0}")
             x, y, n, y_x, n_x = integrate(
                 ideal,
                 dyneemite_pro,
                 T0,
                 theta0,
+                masses=[(5.0, 70.0), (10.0, 80)],
                 length_cutoff=100000.0,
             )
             plt.plot(x, y, label=f"T0={T0}, theta0={theta0}")
