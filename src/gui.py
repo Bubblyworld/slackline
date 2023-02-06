84d8438d25286b417ab3ccb6262140a870a5e2b0
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-02-06 23:42:15 +0200

    Some clean-up before working on the javascript client.

--- src/gui.py
+++
@@ -1,5 +0,0 @@
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-import numpy as np
-import tkinter as tk
--- src/test.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot as plt
-from .api import Constraints, Rig
-
-def main():
-    c = Constraints(gap_length=100, anchor_tension=1000)
-    c.add_slackliner(position=50, mass=90)
-    print(c.to_json())
